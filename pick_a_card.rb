56aa36a0b412519f293ae57ffee4f207712c42bb
RedGem445 <greengem766@gmail.com>
2016-08-12 17:16:26 +0300

    Update pick_a_card.rb

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,7 @@
+#encoding : UTF-8
+
 values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # γ΅¨, —¥ΰΆ¨, ΰ¥αβ¨, ¨ª¨
+suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # ΒΓ£Β΅Β¨, β€”Β¥Γ ΒΆΒ¨, Ε Γ Β¥Γ΅ΓΆΒ¨, ΒΒ¨ΒªΒ¨
 
 puts "How much do you need cards?"
 
