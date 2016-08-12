79108bfe0ce7eef616a9f6aa5ce9012046461a50
GreenGem <greengem766@gmail.com>
2016-08-12 16:49:23 +0300

    Version 2

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,18 @@
 values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Буби, Черви, Крести, Пики
 
-puts "#{values.sample} of #{suits.sample}"
+puts "How much do you need cards?"
+
+num = gets.STDIN.chomp
+
+
+num.times do
+
+  puts "#{values.sample} of #{suits.sample}"
+
+  sleep 1.5
+
+end
+
+
+
